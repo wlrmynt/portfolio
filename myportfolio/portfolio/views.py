8d0c7258a4afc0e2854fbab38b83931477d3bde3
wlrmynt <wmi54968@gmail.com>
2023-11-19 16:43:41 +0700

    menmpilka data

--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -1,14 +1,23 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Detail, Contact, Member
 
 
 def portfolio(request):
+   
   template = loader.get_template('home.html')
   return HttpResponse(template.render())
 
 def detail(request):
+  data_detail = Detail.objects.all()  
   template = loader.get_template('base.html')
-  return HttpResponse(template.render())
+  context = {
+    'salam' : "Halo ini Detail",
+    'details' : data_detail,
+  }
+  return HttpResponse(template.render(context, request))
+
 
 def picture(request):
   template = loader.get_template('picture.html')
